04a03249d8473b065deafd2f19928d33562b5eb9
Michel Esc <eih3.prog@outlook.fr>
2015-06-03 12:09:23 +0200

    Update Emetteur.ino

--- Emetteur.ino
+++ Emetteur.ino
@@ -1,54 +1,50 @@
+#include <dht.h>
 #include <VirtualWire.h>
 
-#define LED    13 
-#define RX_PIN 12
+dht DHT;
 
-String Temp; //Stockage de la température
-String Humid; //Stockage de l'humidité
+#define DHT11_PIN 8
+#define LED       2
+#define TX_PIN    12
+#define DELAY     10 //secondes avant chaque envoi
 
-/**** Fonction configuration ****/
+/**** Fonction configuration*/
 void setup()
 {
   Serial.begin(9600);
+  Serial.println("Station Meteo Arduino Emetteur\n");
   
-  Serial.println("Station Meteo Arduino Emetteur"); 
+  pinMode(LED, OUTPUT);
+
+  vw_set_tx_pin(TX_PIN); 
+  vw_setup(2000); 
   
-  vw_set_tx_pin(RX_PIN);
-  vw_setup(2000);      
-  vw_rx_start();      
+  delay(1000);
 }
 
 void loop()
 {
-  uint8_t buf[VW_MAX_MESSAGE_LEN];
-  uint8_t buflen = VW_MAX_MESSAGE_LEN;
+  int chk = DHT.read11(DHT11_PIN); //Lecture des données DHT11
   
-  if (vw_get_message(buf, &buflen))
-  {
-    digitalWrite(LED, HIGH);
-    
-    char Char[buflen]; 
-    
-    for (int i = 0; i < buflen; i++)
-    {
-      Char[i]  = char(buf[i]); //Stockage du buffer dans un tableau
-    }
-    
-    digitalWrite(LED, LOW);
- 
-    LectureDonnees(Char);   
-  }
+  // Envoi de la trame temp + hum ex: 24.0050.00
+  EnvoiDonnees((String)DHT.temperature + ((String)DHT.humidity));
+  
+  Serial.println("Temperature : " + (String)DHT.temperature + " oC");    
+  Serial.println("Humidite : " + (String)DHT.humidity + " %");    
+  Serial.println("\n"); 
+  
+  delay(DELAY * 1000);
 }
 
-/**** Fonction lecture des données reçus ****/
-void LectureDonnees(char* donnees)
-{  
-  String data = (String)donnees;
-      
-  Temp = data.substring(0, 2);
-  Humid = data.substring(5, 7);
-    
-  Serial.println("Temperature : " + Temp + " oC");    
-  Serial.println("Humidite : " + Humid + " %");    
-  Serial.println("\n");  
+/**** Fonction envoi des données DHT11 ****/
+void EnvoiDonnees(String donnees)
+{
+  const char* data = donnees.c_str();
+  
+  digitalWrite(LED, HIGH);
+
+  vw_send((uint8_t *)data, strlen(data));
+  vw_wait_tx();
+  
+  digitalWrite(LED, LOW);
 }
